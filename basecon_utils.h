ebc6d3ebb0f4940d88b089f7719055403b9d44cb
Ahlamkohli <121582485+Ahlamkohli@users.noreply.github.com>
2022-12-30 20:14:09 +0100

    Create basecon_utils.h

---
+++ basecon_utils.h
@@ -0,0 +1,18 @@
+#ifndef UTILS_H /* Include guard */
+#define UTILS_H
+
+//Function declarations
+int power(int base, int exp);
+int isLower(int ch);
+int isDigit(int n);
+int Strlen(const char* string);
+int Strcmp(const char* string_1, const char* string_2);
+int AtoI(const char* string);
+void reverse(char* string);
+int delete(char* string, int index, int u_size);
+void memreset(char* string, int u_size);
+
+#endif
+
+
+
